08eb6db59c289421b6ab7b6e49b975f5ff67a17d
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-05-07 17:09:51 +0200

    added functions is_signed_in and user_has_role

--- html/index.php
+++ html/index.php
@@ -1,8 +1,17 @@
 <?php
-
+session_start();
+include 'functions.php';
 require_once 'header.php';
 
 echo "<main><p>This is the main content of the page.</p></main>";
-
+if(is_signed_in() && user_has_role('kund')){
+    echo "välkommen, du är i kundläget.";
+} else if(is_signed_in() && user_has_role('prenumerant')){
+    echo "välkommen, du är i prenumerantläget.";
+} else {
+    echo "välkommen, vänligen logga in för att se mer.";
+}
 require_once 'footer.php'; 
+
+//Måste man avsluta någon session? 
 ?>
