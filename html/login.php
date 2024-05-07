c6e9afb3dccb6a97cde6d0c564f1aa751dbce531
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-05-07 17:35:27 +0200

    added functionality for login as customer with redirect

--- html/login.php
+++ html/login.php
@@ -1,8 +1,35 @@
 <?php
+session_start(); 
 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    //Hårdkodade exempel
+    $exampel_email = "test@test.com";
+    $exampel_password = "12345";
+
+    //Hämta från formuläret: 
+    $mail = $_POST['email'];
+    $password = $_POST['password'];
+    
+    //Kolla om match
+    if($mail == $exampel_email && $password == $exampel_password) {
+        $_SESSION['user_id'] = 1;
+        $_SESSION['role'] = "kund";
+        
+        header("Location: index.php");
+        exit;
+    } else {
+        $error_msg = "Fel användarnamn eller lösenord";
+    }
+
+}
 require_once 'header.php';
 
 echo "<main><p>LOG IN</p></main>";
+if(isset($error_msg)) { ?>
+<p style="color: red;"><?php echo $error_msg; ?></p>
+<?php 
+}
 ?> 
 
     <h2>Logga in</h2>
