93735db99c847903c1ac5e49d3edf43a6c531e8e
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-05-07 17:21:07 +0200

    added form for login

--- html/login.php
+++ html/login.php
@@ -3,6 +3,17 @@
 require_once 'header.php';
 
 echo "<main><p>LOG IN</p></main>";
+?> 
 
+    <h2>Logga in</h2>
+    <form method="post">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br><br>
+        <label for="password">Lösenord:</label>
+        <input type="password" id="password" name="password" required><br><br>
+        <input type="submit" value="Logga in">
+    </form>
+
+<?php
 require_once 'footer.php'; 
 ?>
