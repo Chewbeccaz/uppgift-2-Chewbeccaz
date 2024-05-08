802430873d02f68ad8b819bce1eb445631900eb9
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-05-08 15:57:47 +0200

    checking if email exists in db

--- html/mailgun/send-email.php
+++ html/mailgun/send-email.php
@@ -1,40 +1,55 @@
 <?php 
 
+$mysql = new mysqli("db", "root", "notSecureChangeMe", "newsletter_db");
+
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $email = $_POST['email'];
 
-    $random_code = rand(100000, 999999);
+     // Kontrollera om e-postadressen finns i databasen
+     $sql = "SELECT * FROM users WHERE email = ?";
+     $stmt = $mysql->prepare($sql);
+     $stmt->bind_param("s", $email);
+     $stmt->execute();
+     $result = $stmt->get_result();
+ 
+     if ($result->num_rows > 0) {
+        // om epostadressen finns
+        $random_code = rand(100000, 999999);
 
-    $api_key = '6801a1683e8bc05c6caa558441f86332-ed54d65c-90f79098';
-    $domain = 'sandboxf6f87e056e944f6ba17636acf63b82a5.mailgun.org';
-    $url = 'https://api.mailgun.net/v3/'.$domain.'/messages';
+        $api_key = '6801a1683e8bc05c6caa558441f86332-ed54d65c-90f79098';
+        $domain = 'sandboxf6f87e056e944f6ba17636acf63b82a5.mailgun.org';
+        $url = 'https://api.mailgun.net/v3/'.$domain.'/messages';
 
+        $my_message = [
+            'from' => 'Excited User <mailgun@' . $domain . '>',
+            'to' => $email,
+            'subject' => 'Your Random Code',
+            'text' => "En random kod : {$random_code}"
+        ];
 
-    $my_message = [
-        'from' => 'Excited User <mailgun@' . $domain . '>',
-        'to' => $email,
-        'subject' => 'Your Random Code',
-        'text' => "En random kod : {$random_code}"
-    ];
+        $ch = curl_init(); 
 
-    $ch = curl_init(); 
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_USERPWD, "api:{$api_key}");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $my_message);
 
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-    curl_setopt($ch, CURLOPT_USERPWD, "api:{$api_key}");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $my_message);
+        $response = curl_exec($ch);
 
-    $response = curl_exec($ch);
+        curl_close($ch);
 
-    curl_close($ch);
-
-    if ($response === false) {
-        die('Curl failed: ' . curl_error($ch));
+        if ($response === false) {
+            die('Curl failed: ' . curl_error($ch));
+        } else {
+            echo 'Du har fått ett skoj mail i din skräppost!!';
+        }
     } else {
-        echo 'Du har fått ett skoj mail i din skräppost!!';
+        echo "E-postadressen finns inte i databasen.";
     }
+    
+    $stmt->close();
 }
 ?>
 
