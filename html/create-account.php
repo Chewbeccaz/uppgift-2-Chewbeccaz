e11502cc2a81faad4177c46a67c0920ebfe32d13
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-05-15 16:09:21 +0200

    create account with firstname and lastname

--- html/create-account.php
+++ html/create-account.php
@@ -10,6 +10,8 @@ if ($mysql->connect_error) {
 // Kontrollera om formuläret har skickats
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
     $password = $_POST['password'];
     $confirm_password = $_POST['confirm_password'];
     $role = $_POST['role'];
@@ -23,9 +25,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   
     // $password_hash = password_hash($password, PASSWORD_DEFAULT); ändra sen. 
 
-    $sql = "INSERT INTO users (email, password, role) VALUES (?,?,?)";
+    $sql = "INSERT INTO users (email, firstname, lastname, password, role) VALUES (?,?,?,?,?)";
     $stmt = $mysql->prepare($sql);
-    $stmt->bind_param("sss", $email, $password, $role);
+    $stmt->bind_param("sssss", $email, $firstname, $lastname, $password, $role);
 
     if ($stmt->execute()) {
         echo "Användare skapad";
@@ -53,7 +55,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     $stmt->close();
-    $stmtNewsletter->close(); 
+    if (isset($stmtNewsletter)) { 
+        $stmtNewsletter->close();
+    }
     $mysql->close();
 } else {
     // Om formuläret inte har skickats, visa det igen
@@ -64,6 +68,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <h3>SKAPA KONTO:</h3>
         <form method="post">
             E-post: <input type="email" name="email" required><br>
+            Förnamn: <input type="text" name="firstname" required><br>
+            Efternamn: <input type="text" name="lastname" required><br>
             Lösenord: <input type="password" name="password" required><br>
             Bekräfta lösenord: <input type="password" name="confirm_password" required><br>
             Roll: <select name="role" required>
