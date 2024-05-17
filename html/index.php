d4784a995771fdce3f08367abb178f1b628f0f9b
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-05-17 11:03:44 +0200

    styled header and footer and main

--- html/index.php
+++ html/index.php
@@ -3,7 +3,24 @@ session_start();
 include 'functions.php';
 include_once './components/header.php';
 
-echo "<main><p>This is the main content of the page.</p></main>";
+?>
+
+<main>
+        <p class="welcome-message">This is the main content of the page.</p>
+        <?php
+        if(is_signed_in() && user_has_role('kund')){
+            echo "Välkommen, du är i kundläget.";
+        } else if(is_signed_in() && user_has_role('prenumerant')){
+            echo "Välkommen, du är i prenumerantläget.";
+        } else {
+            echo "Välkommen, vänligen logga in för att se mer här.";
+        }
+       ?>
+    </main>
+
+    <?php include_once './components/footer.php';?>
+
+<!-- echo "<main><p class='welcome-message'>This is the main content of the page.</p></main>";
 if(is_signed_in() && user_has_role('kund')){
     echo "välkommen, du är i kundläget.";
 } else if(is_signed_in() && user_has_role('prenumerant')){
@@ -13,5 +30,4 @@ if(is_signed_in() && user_has_role('kund')){
 }
 include_once './components/footer.php'; 
 
-//Måste man avsluta någon session? 
-?>
+?> -->
